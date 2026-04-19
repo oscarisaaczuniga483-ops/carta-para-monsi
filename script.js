9cf3f578840e771d67b818f50d90a3b78fb5751e
oscarisaaczuniga483-ops <oscarisaaczuniga483@gmail.com>
2026-04-19 14:38:52 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function interactuar(elemento) {
-    // Revelar la foto al tocar
+   
     elemento.classList.add('revelada');
 
-    // Reproducir música
+    
     const cancion = document.getElementById('musica-cumple');
     cancion.play().catch(error => {
         console.log("El navegador bloqueó el audio, pero sonará al siguiente toque.");
@@ -13,13 +13,13 @@ function revelarCarta() {
     const pastel = document.getElementById('contenedor-pastel');
     const carta = document.getElementById('contenedor-carta');
 
-    // Efecto de desvanecimiento
+ 
     pastel.style.opacity = '0';
     
     setTimeout(() => {
         pastel.style.display = 'none';
         carta.classList.remove('oculto');
-        // Pequeña animación de entrada para la carta
+       
         carta.style.animation = 'fadeIn 1s forwards';
     }, 500);
 }
